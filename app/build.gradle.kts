fc5269e708deb39f09b74b84380b1f43d576c5d2
Miguel Caldas <23634895+MiguelCaldasMS@users.noreply.github.com>
2026-09-03 00:01:23 +0100

    Prepare 1.0.4 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.miguelcaldas.mcsmsforwardermultichannel"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 3
-        versionName = "1.0.3"
+        versionCode = 4
+        versionName = "1.0.4"
     }
 
     signingConfigs {
